f6fce631a5e957c025c0f8a3951372ecbe8b1d18
Matxxsito <150559018+Matxxsito@users.noreply.github.com>
2026-02-06 16:12:24 -0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,23 @@
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-storage-compat.js"></script>
 
+<script>
+const firebaseConfig = {
+  apiKey: "AIzaSyCvwYztzOuW5CQ4rCWSgNFLhAifIjxB5eo",
+  authDomain: "miaupedia-11d46.firebaseapp.com",
+  projectId: "miaupedia-11d46",
+  storageBucket: "miaupedia-11d46.firebasestorage.app",
+  messagingSenderId: "616954573945",
+  appId: "1:616954573945:web:4e5ad40fa3927b518650a8",
+  measurementId: "G-YFK9Z79C83"
+
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+</script>

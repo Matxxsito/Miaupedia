bae378813240090a433459fb0a4e6492251db1d2
Matxxsito <150559018+Matxxsito@users.noreply.github.com>
2026-02-06 17:53:09 -0500

    Update foro.js

--- js/foro.js
+++ js/foro.js
@@ -1,1 +1,33 @@
+import { auth, db, storage } from "./firebase.js";
+import {
+  collection,
+  addDoc,
+  query,
+  orderBy,
+  onSnapshot,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
+// ELEMENTOS
+const mensaje = document.getElementById("mensaje");
+const imagen = document.getElementById("imagen");
+const publicarBtn = document.getElementById("publicarBtn");
+const posts = document.getElementById("posts");
+
+// 🔐 PROTEGER EL FORO
+auth.onAuthStateChanged(user => {
+  if (!user) {
+    window.location.href = "login.html";
+  }
+});
+
+// ✅ BOTÓN FUNCIONA (PRUEBA REAL)
+publicarBtn.addEventListener("click", async () => {
+  alert("EL BOTÓN FUNCIONA ✅");
+});

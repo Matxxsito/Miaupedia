54a1301df8aaa9010cd7c48c4ad28dc5778d38af
Matxxsito <150559018+Matxxsito@users.noreply.github.com>
2026-02-06 17:00:40 -0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,19 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from
+"https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const loginBtn = document.getElementById("loginBtn");
+
+if (loginBtn) {
+  loginBtn.addEventListener("click", () => {
+    signInWithEmailAndPassword(auth, email.value, password.value)
+      .then(() => {
+        window.location.href = "foro.html";
+      })
+      .catch(error => {
+        alert(error.message);
+      });
+  });
+}
